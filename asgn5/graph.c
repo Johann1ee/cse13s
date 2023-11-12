b3b6fafea8c890a937e7f626c22593f03f27139a
Johann <jobylee@ucsc.edu>
2023-11-12 07:58:30 +0000

    helper functions files done

---
+++ asgn5/graph.c
@@ -0,0 +1,101 @@
+#include "graph.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct graph {
+	uint32_t vertices;
+	bool directed;
+	bool *visited;
+	char **names;
+	uint32_t **weights;
+} Graph;
+
+Graph *graph_create(uint32_t vertices, bool directed){
+	Graph *g = calloc(1, sizeof(Graph));
+	g->vertices = vertices;
+	g->directed = directed;
+	
+	// use calloc to initialize everything with zeroes
+	g->visited = calloc(vertices, sizeof(bool));
+	g->names = calloc(vertices, sizeof(char *));
+
+	//allocate g->weights with a pointer for each row
+	g->weights = calloc(vertices, sizeof(g->weights[0]));
+
+	//allocate each row in the adjacency matrix
+	for (uint32_t i = 0; i < vertices; i++) {
+		g->weights[i] = calloc(vertices, sizeof(g->weights[0][0]));
+	}
+
+	return g;
+}
+
+void graph_free(Graph **gp){
+	if (*gp) {
+
+		for (uint32_t i = 0; i < (*gp)->vertices; i++){
+			if((*gp)->weights[i]){
+				free((*gp)->weights[i]);
+			}
+		}
+
+		if ((*gp)->weights){
+			free((*gp)->weights);
+		}
+
+		if ((*gp)->names){
+			free((*gp)->names);
+		}
+
+		if ((*gp)->visited){
+			free((*gp)->visited);
+		}
+
+		free(*gp);
+	}
+}
+
+uint32_t graph_vertices(const Graph *g){
+	return g->vertices;
+}
+
+void graph_add_vertex(Graph *g, const char *name, uint32_t v){
+	if (g->names[v]) free(g->names[v]);
+	g->names[v] = strdup(name);
+}
+
+const char* graph_get_vertex_name(const Graph *g, uint32_t v){
+	return g->names[v];
+}
+
+char **graph_get_names(const Graph *g){
+	return g->names;
+}
+
+void graph_add_edge(Graph *g, uint32_t start, uint32_t end, uint32_t weight){
+	g->weights[start][end] = weight;
+	//Next line is assuming undirected
+	g->weights[end][start] = weight;
+}
+
+uint32_t graph_get_weight(const Graph *g, uint32_t start, uint32_t end){
+	return g->weights[start][end];
+}
+
+void graph_visit_vertex(Graph *g, uint32_t v){
+	g->visited[v] = true;
+}
+
+void graph_unvisit_vertex(Graph *g, uint32_t v){
+	g->names[v] = false;
+}
+
+bool graph_visited(const Graph *g, uint32_t v){
+	if (g->visited[v]){
+		return true;
+	}else{
+		return false;
+	}
+}
+
---
+++ asgn5/path.c
@@ -0,0 +1,81 @@
+#include "path.h"
+
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct path {
+	uint32_t total_weight;
+	Stack *vertices;
+} Path;
+
+Path *path_create(uint32_t capacity){
+	Path *createPath = (Path *)malloc(sizeof(Path));
+	createPath->total_weight = 0;
+	createPath->vertices = stack_create(capacity);
+	return createPath;
+}
+
+void path_free(Path **pp){
+	stack_free(&(*pp)->vertices);
+	
+	if (*pp){
+		free(*pp);
+	}
+}
+
+
+uint32_t path_vertices(const Path *p){
+	uint32_t *topper = NULL;
+	stack_peek(p->vertices, topper);
+	return *topper;
+}
+
+uint32_t path_distance(const Path *p){
+	return p->total_weight;
+}
+
+void path_add(Path *p, uint32_t val, const Graph *g){
+	if (stack_empty(p->vertices)){
+		p->total_weight = 0;
+		stack_push(p->vertices, val);
+	} else{
+		uint32_t *holder = NULL;
+		stack_peek(p->vertices, holder);
+		stack_push(p->vertices, val);
+
+		p->total_weight += graph_get_weight(g, *holder, val);
+	}
+}
+
+uint32_t path_remove(Path *p, const Graph *g){
+	uint32_t *holder = NULL;
+	stack_peek(p->vertices, holder);
+
+	if (stack_size(p->vertices) <= 2){
+		stack_pop(p->vertices, holder);
+		p->total_weight = 0;
+	} else{
+		stack_pop(p->vertices, holder);
+
+		uint32_t *temp = NULL;
+		stack_peek(p->vertices, temp);
+		p->total_weight -= graph_get_weight(g, *temp, *holder);
+	}
+
+	return *holder;
+}
+
+void path_copy(Path *dst, const Path *src){
+	dst->total_weight = src->total_weight;
+	stack_copy(dst->vertices, src->vertices);
+}
+
+void path_print(const Path *p, FILE *f, const Graph *g){
+	for (uint32_t i = 0; i < path_vertices(p); i++){
+		const char *name = graph_get_vertex_name(g, i);
+
+		fprintf(f, "%s\n", name);
+	}
+
+	fprintf(f, "\n");
+}
