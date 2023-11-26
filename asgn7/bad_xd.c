001195d0ba5182b24dd9555102d1f20cbf0e4ca8
Johann <jobylee@ucsc.edu>
2023-11-26 09:11:19 +0000

    i forgot to add character count

--- asgn7/bad_xd.c
+++ asgn7/bad_xd.c
@@ -1,29 +1,25 @@
-#include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
 #define n size_t
 #define a 16
 #define m printf
 #define y c[i]
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 void z(char c[a], int f, n e) {
     m("%08x: ", f);
     for (n i = 0; i < a; i++) {
         m((i < e) ? "%02x" : "  ", (unsigned char) y);
         m((i % 2 == 1) ? " " : 0);
     }
-    for (n i = 0; i < a; i++)
-        y = (y < 32 || y >= 127) ? '.' : y;
     m(" ");
     for (n i = 0; i < e; i++)
-        putchar(y);
+        (y < 32 || y >= 127) ? putchar('.') : putchar(y);
     m("\n");
 }
 int main(int g, char *h[]) {
-    int b = 0, f = 0;
+    int b = (g == 2) ? open(h[1], O_RDONLY, 0) : 0, f = 0;
     char c[a];
-    n d = 0, e = 0;
-    if (g == 2)
-        b = open(h[1], O_RDONLY, 0);
+    n d, e = 0;
     if (b == -1)
         return 1;
     while ((d = (n) read(b, c + e, a - e)) > 0) {
@@ -34,9 +30,8 @@ int main(int g, char *h[]) {
             f += a;
         }
     }
-    if (e > 0) {
+    if (e > 0)
         z(c, f, e);
-    }
     close(b);
     return 0;
 }
